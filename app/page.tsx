0255f6f928b5fbecef9376e744880eeada277232
Amjuz <137517288+amjuz@users.noreply.github.com>
2024-10-14 19:19:35 +0530

    First Commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen font-bold text-4xl">
+      Initializing Project
+    </div>
+  );
+}
